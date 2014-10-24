df1b17460cb4ff0cdc45002e194057e95f4a4230
Merlijn Wajer <merlijn@wizzup.org>
2014-10-24 15:52:26 +0200

    Add GPLv3 license.

--- graphs.py
+++ graphs.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python2
+"""
+This file is part of the TechInc Financial Administration
+Copyright (c) 2014 by Merlijn Wajer
+
+TInance is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TInance is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TInance.  If not, see <http://www.gnu.org/licenses/>.
+
+See the file COPYING, included in this distribution,
+for details about the copyright.
+"""
+
+
 import pylab
 
-from sql import *
+import tidb
+from tidb.sql import *
 
 """
 DONE:
--- tidb/classes.py
+++ tidb/classes.py
@@ -1,3 +1,24 @@
+"""
+This file is part of the TechInc Financial Administration
+Copyright (c) 2014 by Merlijn Wajer
+
+TInance is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TInance is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TInance.  If not, see <http://www.gnu.org/licenses/>.
+
+See the file COPYING, included in this distribution,
+for details about the copyright.
+"""
+
 from sqlalchemy import Table, Column, Integer, String, Date, func, \
      ForeignKey, Unicode, Boolean, Numeric
 
